c7ac13ff01a74775ef7bc58a3388157f0905eb94
Josh Sherick <jsherick@qualtrics.com>
2016-07-01 15:30:59 -0600

    adds some vim plugins

--- .vim/bundle/clone.sh
+++ .vim/bundle/clone.sh
@@ -6,6 +6,9 @@ REPOS+='vim-airline/vim-airline-themes '
 REPOS+='tpope/vim-surround '
 REPOS+='junegunn/goyo.vim '
 REPOS+='tpope/vim-repeat '
+REPOS+='tpope/vim-fugitive '
+REPOS+='ctrlpvim/ctrlp.vim '
+REPOS+='tyru/current-func-info.vim '
 
 for REPO in $REPOS; do
     git clone https://github.com/$REPO
