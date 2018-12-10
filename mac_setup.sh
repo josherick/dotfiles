5bc4c5acfd88b459ea61d1a2406e8d5651cce52d
Joshua Sherick <jsherick@qualtrics.com>
2018-12-10 13:47:51 -0800

    reattach-to-user-namespace no longer needed

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh tmux coreutils vim gnu-sed reattach-to-user-namespace openssl fzf tree rsync
+brew install zsh tmux coreutils vim gnu-sed openssl fzf tree rsync
 brew cask install alacritty
 sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
