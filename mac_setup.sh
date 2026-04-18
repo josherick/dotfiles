74ccbe1a327e027f0915d34e987751a692ad0b0e
Josh Sherick <josh@sherick.me>
2026-04-18 11:51:54 -0400

    update homebrew path

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,5 +2,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install zsh tmux coreutils vim gnu-sed openssl fzf tree rsync
 brew install --cask alacritty
-sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
-chsh -s /usr/local/bin/zsh
+sudo bash -c 'echo "/opt/homebrew/bin/zsh" >> /etc/shells'
+chsh -s /opt/homebrew/bin/zsh
